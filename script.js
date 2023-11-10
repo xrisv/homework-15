84bf3ea375671c4b518e6b599a1a4cb872eb91cf
Lazar <146124647+xrisv@users.noreply.github.com>
2023-11-10 17:41:45 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+let numberVar = 42;
+let stringVar = "Hello, World!"; 
+let booleanVar = true;
+
+console.log(numberVar, stringVar, booleanVar);
+
+let a = 10;
+let b = 5;
+
+let sum = a + b;
+let difference = a - b;
+let product = a * b;
+let quotient = a / b;
+
+console.log("Sum:", sum);
+console.log("Difference:", difference);
+console.log("Product:", product);
+console.log("Quotient:", quotient);
+
+let firstName = "John";
+let lastName = "Doe";
+
+let fullNameConcatenation = firstName + " " + lastName;
+let fullNameTemplateLiteral = `${firstName} ${lastName}`;
+
+console.log("Concatenation:", fullNameConcatenation);
+console.log("Template Literal:", fullNameTemplateLiteral);
+
+let length = 7;
+let width = 5;
+
+let area = length * width;
+
+console.log("მართკუთხედის ფართობი")
